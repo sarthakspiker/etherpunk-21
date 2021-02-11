52cb0f264578894481ce67b94872c65ba2f993f0
Prateek Jha <prateekj777@gmail.com>
2021-02-12 00:09:15 +0530

    Updates

---
+++ precache-manifest.b435d00a2feb27d26fc0907b32844741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b89777f671a156ebbd5a9dab482eeb8",
+    "url": "/etherpunk-21/index.html"
+  },
+  {
+    "revision": "e57936e826a2655f1461",
+    "url": "/etherpunk-21/static/css/2.37c27f5c.chunk.css"
+  },
+  {
+    "revision": "5be59ee78fc1711b84b7",
+    "url": "/etherpunk-21/static/css/main.2bac8dab.chunk.css"
+  },
+  {
+    "revision": "e57936e826a2655f1461",
+    "url": "/etherpunk-21/static/js/2.fcdd2d61.chunk.js"
+  },
+  {
+    "revision": "5be59ee78fc1711b84b7",
+    "url": "/etherpunk-21/static/js/main.550a775a.chunk.js"
+  },
+  {
+    "revision": "96b25c72a83f5439eb5f",
+    "url": "/etherpunk-21/static/js/runtime-main.ce635d88.js"
+  }
+]);
