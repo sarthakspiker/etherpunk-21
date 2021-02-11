4a132e7ca6cb29194af5c4eef145fcb283812e80
Prateek Jha <prateekj777@gmail.com>
2021-02-11 23:51:24 +0530

    Added gh-pages for deployment on github

--- dland/client/src/App.js
+++ dland/client/src/App.js
@@ -85,26 +85,9 @@ class App extends Component {
   };
 
   render() {
-    // if (!this.state.web3) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
-    // return (
-    //   <div className="App">
-    //     <h1>Good to Go!</h1>
-    //     <p>Your Truffle Box is installed and ready.</p>
-    //     <h2>Smart Contract Example</h2>
-    //     <p>
-    //       If your contracts compiled and migrated successfully, below will show
-    //       a stored value of 5 (by default).
-    //     </p>
-    //     <p>
-    //       Try changing the value stored on <strong>line 40</strong> of App.js.
-    //     </p>
-    //     <div>The stored value is: {this.state.storageValue}</div>
-    //     <button onClick={this.runExample}>Send 5 GWEI</button>
-    //     <h3>Transaction Hash is: {this.state.transactionHash}</h3>
-    //   </div>
-    // );
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
     return (
       <div className="App">
         <Router>
